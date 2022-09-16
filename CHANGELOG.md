38cafdbdd760cf75b824b57da4d9e1b5b2c7e34a
Dominik <39081942+ufo512@users.noreply.github.com>
2022-09-16 23:55:56 +0200

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+## [0.4.2](https://pub.dartlang.org/packages/webfeed/versions/0.4.2)
+### Fixed
+- Bad import in `rss_content.dart` & `rss_source.dart`
+## [0.4.1](https://pub.dartlang.org/packages/webfeed/versions/0.4.1)
+### Added
+- Support `author` in RssFeed
+## [0.4.0](https://pub.dartlang.org/packages/webfeed/versions/0.4.0)
+### Added
+- Support for dublin core namespace
+- Support enclosure in rss item
+- Set minimum dart version into 2
+
+## [0.3.0](https://pub.dartlang.org/packages/webfeed/versions/0.3.0)
+### Added
+- Support for image namespace
---
+++ lib/domain/rss_item.dart
@@ -0,0 +1,61 @@
+import 'package:webfeed/domain/dublin_core/dublin_core.dart';
+import 'package:webfeed/domain/media/media.dart';
+import 'package:webfeed/domain/rss_category.dart';
+import 'package:webfeed/domain/rss_content.dart';
+import 'package:webfeed/domain/rss_enclosure.dart';
+import 'package:webfeed/domain/rss_source.dart';
+import 'package:webfeed/util/helpers.dart';
+import 'package:xml/xml.dart';
+
+class RssItem {
+  final String title;
+  final String description;
+  final String link;
+
+  final List<RssCategory> categories;
+  final String guid;
+  final String pubDate;
+  final String author;
+  final String comments;
+  final RssSource source;
+  final RssContent content;
+  final Media media;
+  final RssEnclosure enclosure;
+  final DublinCore dc;
+
+  RssItem({
+    this.title,
+    this.description,
+    this.link,
+    this.categories,
+    this.guid,
+    this.pubDate,
+    this.author,
+    this.comments,
+    this.source,
+    this.content,
+    this.media,
+    this.enclosure,
+    this.dc,
+  });
+
+  factory RssItem.parse(XmlElement element) {
+    return RssItem(
+      title: findElementOrNull(element, "title")?.text,
+      description: findElementOrNull(element, "description")?.text,
+      link: findElementOrNull(element, "link")?.text,
+      categories: element.findElements("category").map((element) {
+        return RssCategory.parse(element);
+      }).toList(),
+      guid: findElementOrNull(element, "guid")?.text,
+      pubDate: findElementOrNull(element, "pubDate")?.text,
+      author: findElementOrNull(element, "author")?.text,
+      comments: findElementOrNull(element, "comments")?.text,
+      source: RssSource.parse(findElementOrNull(element, "source")),
+      content: RssContent.parse(findElementOrNull(element, "content:encoded")),
+      media: Media.parse(element),
+      enclosure: RssEnclosure.parse(findElementOrNull(element, "enclosure")),
+      dc: DublinCore.parse(element),
+    );
+  }
+}
